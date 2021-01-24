03db37990c3cba1140d8644b659c31dbe0af2b7c
Pavel Bely <poul-white@yandex.ru>
2021-01-24 18:58:25 +0300

    feat: update .gitignore

---
+++ pages/mobile/404.tsx
@@ -0,0 +1,10 @@
+import { DesktopPageLayout } from '@modules/core/common/desktop-page-layout/desktop-page-layout';
+import { NotFoundView } from '@modules/core/common/not-found-view/not-found-view';
+
+export default function NotFoundPage() {
+  return (
+    <DesktopPageLayout>
+      <NotFoundView />
+    </DesktopPageLayout>
+  );
+}
---
+++ pages/mobile/_app.tsx
@@ -0,0 +1,14 @@
+import { GlobalStyle } from '@modules/core/global.styled';
+import { useFontLoader } from '@modules/core/hooks/use-font-loader';
+
+export default function App({ Component, pageProps }) {
+  useFontLoader();
+
+  return (
+    <>
+      <GlobalStyle />
+
+      <Component {...pageProps} />
+    </>
+  );
+}
